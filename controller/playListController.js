ff4bf82296cf06371c334cfbeef90dc7a077144b
kendallReyes <kendall14solr@gmail.com>
2025-04-14 21:39:10 -0600

    Implementation of the getPlayListByAdminUser method added

--- controller/playListController.js
+++ controller/playListController.js
@@ -6,14 +6,25 @@ const AdminUser = require("../models/adminUser");
 const getPlayListByRestrictedUser = async ({ id }) => {
     try {
         //const playLists = await PlayList.find({restrictedUsers: new mongoose.Types.ObjectId(id)});
-        const playLists = await PlayList.find({ restrictedUsers: id }).populate("restrictedUsers").populate("adminId");;
+        const playLists = await PlayList.find({ restrictedUsers: id }).populate("restrictedUsers").populate("adminId");
         return playLists;
     }catch (err) {
+        console.error("Error to get play list of this restricted user:", err);
+        throw new Error("Play list of restricted user Id not fount.");
+    }
+}
+
+const getPlayListByAdminUser = async ({ id }) => {
+    try {
+        const playlists = await PlayList.find({ adminId: id }).populate("restrictedUsers").populate("adminId");
+        return playlists;
+    }catch (err) {
         console.error("Error to get restricted user:", err);
-        throw new Error("Restricted user Id not fount.");
+        throw new Error("Play list of the admin user Id not fount.");
     }
 }
 
 module.exports = {
-    getPlayListByRestrictedUser
+    getPlayListByRestrictedUser,
+    getPlayListByAdminUser
 }
